132a5053acda471091117a0ce10bcce3342ce6f7
Mat Booth <mat.booth@gmail.com>
2024-01-30 00:03:50 +0000

    First stab at automated deployment of the server
    Uses nginx for serving static files and proxying to application services

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -n "$INITIAL" ] ; then
+	ansible-playbook -u $USER -k -K -i hosts --tags initial "$@" playbook.yml
+else
+	ansible-playbook -u ansible-maint -i hosts --skip-tags initial "$@" playbook.yml
+fi
