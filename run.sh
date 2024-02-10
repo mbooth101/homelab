d093018312db6fda4857ca531274772c1366e52a
Mat Booth <mat.booth@gmail.com>
2024-02-10 17:07:46 +0000

    Disable pubkey auth for the first connection
    After the initial run is done, it will be pubkey only and password
    auth will be disabled permanantly.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n "$INITIAL" ] ; then
-	ansible-playbook -u $USER -k -K -i hosts --tags initial "$@" playbook.yml
+	ANSIBLE_SSH_COMMON_ARGS="-o PubkeyAuthentication=no" ansible-playbook -u $USER -k -K -i hosts --tags initial "$@" playbook.yml
 else
 	ansible-playbook -u ansible-maint -i hosts --skip-tags initial "$@" playbook.yml
 fi
