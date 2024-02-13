27920dfa6966ff07179944744194fa6af5c05659
Mat Booth <mat.booth@gmail.com>
2024-02-13 20:40:27 +0000

    Remove old and re-add new host keys
    Before intalling new server, to avoid scary SSH messages

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+SERVER_HOST=major-clanger.matbooth.co.uk
+
 if [ -n "$INITIAL" ] ; then
-	ANSIBLE_SSH_COMMON_ARGS="-o PubkeyAuthentication=no" ansible-playbook -u $USER -k -K -i hosts --tags initial "$@" playbook.yml
+	ssh-keygen -R $SERVER_HOST
+	ssh-keyscan -H $SERVER_HOST >> ~/.ssh/known_hosts
+	ANSIBLE_SSH_COMMON_ARGS="-o PubkeyAuthentication=no" ansible-playbook -u $USER -k -K \
+		-i $SERVER_HOST, --tags initial "$@" playbook.yml
 else
-	ansible-playbook -u ansible-maint -i hosts --skip-tags initial "$@" playbook.yml
+	ansible-playbook -u ansible-maint \
+		-i $SERVER_HOST, --skip-tags initial "$@" playbook.yml
 fi
