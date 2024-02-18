998f226b0e4b74fad5989e4b5ee2d7a21b90091c
Mat Booth <mat.booth@gmail.com>
2024-02-18 11:12:06 +0000

    Various improvements
    * Better parameterise host specific vars
    * Rename initial role
    * Allow running against staging or production

--- run.sh
+++ run.sh
@@ -1,13 +1,54 @@
 #!/bin/bash
 
-SERVER_HOST=major-clanger.matbooth.co.uk
+set -e
+
+# Check if running for staging or production
+HOSTS=${PROD:+production}
+if [ -z "$HOSTS" ] ; then
+	HOSTS="staging"
+fi
+
+# Give time to change our minds
+MSG="Running For \"$HOSTS\" Hosts"
+echo
+printf '!%.0s' $(seq 1 $((${#MSG} + 8))) ; echo
+echo "!!! $MSG !!!"
+printf '!%.0s' $(seq 1 $((${#MSG} + 8))) ; echo
+echo
+if [ "$HOSTS" = "staging" ] ; then
+	echo "Hit Ctrl+C and set PROD in the environment to run for production"
+elif [ "$HOSTS" = "production" ] ; then
+	echo "Hit Ctrl+C and unset PROD in the environment to run for staging"
+fi
+echo
+sleep 3
+
+# Determine if this inventory has been initialised already
+INITIAL=1
+if [ -f .initialised ] ; then
+	while read i; do
+		if [ "$i" = "$HOSTS" ] ; then
+			INITIAL=
+			break
+		fi
+	done <.initialised
+fi
 
 if [ -n "$INITIAL" ] ; then
-	ssh-keygen -R $SERVER_HOST
-	ssh-keyscan -H $SERVER_HOST >> ~/.ssh/known_hosts
+	# Reset keys for hosts that need initialising
+	while read h; do
+		host=$(echo "$h" | cut -f1 -d' ')
+		ssh-keygen -R $host
+		ssh-keyscan -H $host >> ~/.ssh/known_hosts
+	done <$HOSTS
+
+	# Initial run must prompt for passwords to set up public key authentication
 	ANSIBLE_SSH_COMMON_ARGS="-o PubkeyAuthentication=no" ansible-playbook -u $USER -k -K \
-		-i $SERVER_HOST, --tags initial "$@" playbook.yml
+		-i $HOSTS --tags initial "$@" playbook.yml
+	echo "$HOSTS" >> .initialised
 else
+	# Subsequent runs as the ansible user using public key authentication
+	# and skips the initial setup roles
 	ansible-playbook -u ansible-maint \
-		-i $SERVER_HOST, --skip-tags initial "$@" playbook.yml
+		-i $HOSTS --skip-tags initial "$@" playbook.yml
 fi
