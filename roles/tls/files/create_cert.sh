2994b7925f735dab2d2332d61cb88307917bc4bd
Mat Booth <mat.booth@gmail.com>
2025-02-14 16:59:00 +0000

    TLS/HTTP role rework
    Now successfully provisioning real certs from letsencrypt

--- roles/tls/files/create_cert.sh
+++ roles/tls/files/create_cert.sh
@@ -3,18 +3,20 @@
 set -e
 
 HOST="$1"
-DOMAIN="$2"
-SUBJECT="/C=GB/ST=South Yorkshire/L=Sheffield/O=Mat Booth Ltd/CN=$HOST.$DOMAIN"
+SUBJECT="/C=GB/ST=South Yorkshire/L=Sheffield/O=Mat Booth Ltd/CN=$HOST"
 
 DIR="$(readlink -f $(dirname $0))"
 DIR_HOST="$DIR/$HOST"
+
+# Just a count of how many certs we've already issued
+SERIAL="$( printf "%02g" $(( $(find . -mindepth 1 -maxdepth 1 -type d | wc -l) + 1 )) )"
 
 if [ ! -d $DIR_HOST ] ; then
 	mkdir $DIR_HOST
 	openssl genrsa -out $DIR_HOST/privkey.pem 4096
 	openssl req -new -key $DIR_HOST/privkey.pem -out $DIR_HOST/cert.csr -subj "$SUBJECT"
 	openssl x509 -req -days 36500 -in $DIR_HOST/cert.csr -CA $DIR/CA.pem -CAkey $DIR/CA.key \
-		-set_serial 01 -out $DIR_HOST/cert.pem
+		-set_serial $SERIAL -out $DIR_HOST/cert.pem
 	rm $DIR_HOST/cert.csr
 	cat $DIR/CA.pem > $DIR_HOST/chain.pem
 	cat $DIR_HOST/cert.pem $DIR_HOST/chain.pem > $DIR_HOST/fullchain.pem
